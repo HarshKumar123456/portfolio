3145c19da2ef3136c009a51aa328521486f00b6c
Harsh Kumar <harshkumar92200@gmail.com>
2025-07-15 09:04:49 +0530

    New Simple Layout & Upgraded to Tailwind Version 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-import questionMark from "../src/assets/questionMark.png";
-import rectangles from "../src/assets/rectangles.png";
-import plusIcon from "../src/assets/plusIcon.png";
-import forwardArrowIcon from "../src/assets/forwardArrowIcon.png";
-import backwardArrowIcon from "../src/assets/backwardArrowIcon.png";
-import placeholderImage from "../src/assets/placeholderImage.png";
-import myPhoto from "../src/assets/myPhoto.jpg";
 import gitHubIcon from "../src/assets/github-icon.png";
 import linkedinIcon from "../src/assets/linkedin-icon.png";
 import gMailIcon from "../src/assets/gmail-icon.png";
@@ -40,52 +33,21 @@ function App() {
     "box-shadow": "rgb(10, 10, 10) 14px 16px 67px 8px, rgb(72, 91, 113) -8px -16px 50px -16px",
   };
 
-  const galleryButtonStyle = {
-    "box-shadow": "0px 4px 10px 2px #00000040 inset",
-  };
-
-  const addImageButtonStyle = {
-    "background": "#FFFFFF08",
-    "box-shadow": "0 3.26px 3.26px #ffffff26 inset,0 0 48.91px #ffffff0d inset,9px 10px 7.1px #0006,-.5px -.5px 6.9px #ffffff40",
-  };
-
-  const forwardBackwardButtonStyle = {
-    "background": "linear-gradient(139.14deg, #303439 12.4%, #161718 94.96%)",
-    "box-shadow": "4px 5px 30px 5px #101213, -5px -3px 30px -10px #96bee7",
-  };
-
 
   const [activeButton, setActiveButton] = useState("about");
 
-  // State to store uploaded images
-  const [images, setImages] = useState([]);
-
-  // Function to handle image selection
-  const handleImageUpload = (event) => {
-    const selectedFiles = Array.from(event.target.files);
-    const imageUrls = selectedFiles.map((file) => URL.createObjectURL(file));
-
-    setImages((prevImages) => [...prevImages, ...imageUrls]); // Append new images to the state
-  };
-
-
   return (
     <>
-      <div className="bg-[#1e1e1e] p-4 min-h-screen">
-
-        <div className="main-container py-14 px-2 md:ps-4 md:pe-12 grid md:grid-cols-2 md:grid-cols-1 gap-8 rounded-3xl" style={mainContainerStyle}>
+      <div className="h-screen flex items-center justify-center p-4">
+        <div className="main-container h-full py-14 px-2 md:ps-4 md:pe-12 py-4 grid md:grid-cols-2 md:grid-cols-1 gap-8 rounded-3xl" style={mainContainerStyle}>
 
           <div className="right-container hidden md:block"></div>
           <div className="left-container">
 
-            <div className="info-card flex px-2 py-4 rounded-lg bg-[#363C43] text-[#969696]" style={infoCardStyle}>
-              <div className="question-mark-and-reactagles me-3 min-w-[16px] max-h-fit">
-                <img src={questionMark} alt="image" className='pb-12' />
-                <img src={rectangles} alt="image" />
-              </div>
+            <div className="info-card h-96 overflow-y-scroll flex px-2 py-4 rounded-lg bg-[#363C43] text-[#969696]" style={infoCardStyle}>
 
               <div className="info">
-                <div className="controls rounded-2xl p-1 lg:me-6 mb-4 grid grid-cols-3" style={controlsStyle}>
+                <div className="controls rounded-2xl p-1 lg:mx-6 mb-4 grid grid-cols-3" style={controlsStyle}>
                   <button className='py-2 text-sm lg:text-base rounded-xl hover:bg-[#343435] hover:text-white' style={activeButton === "about" ? activeButtonStyle : {}} onClick={() => {
                     setActiveButton("about");
                   }}>
@@ -103,7 +65,7 @@ function App() {
                   </button>
                 </div>
 
-                <div className='max-h-32 overflow-y-scroll pt-2 pe-2'>
+                <div className='overflow-y-scroll mx-6 pt-2 pe-2'>
                   {
                     activeButton === "about" ?
                       <>
@@ -116,7 +78,7 @@ function App() {
                             Harsh Kumar
                           </b>
                         </a> and I am a Full-stack Web Developer with hands-on experience in building scalable web applications.
-                        Currently working as an <b>SDE</b> Intern at <i><b>Salahkaar Consultants</b></i>, my approach to development is
+                        My approach to development is
                         grounded in continuous learning and adapting to emerging technologies. With a belief in the
                         Cosmopolitan Ideology, I strive to contribute to a diverse and inclusive work environment
                         where ideas from all backgrounds are welcomed and nurtured.
@@ -130,7 +92,7 @@ function App() {
                           <br />
                           <i>
                             <b>
-                              (July 2024 - Present)
+                              (July 2024 - October 2024)
                             </b>
                           </i>
                           <br />
@@ -148,15 +110,15 @@ function App() {
                           <br />
                           <br />
                           Mail me @ <a href="mailto:harshkumar92200@gmail.com" className='text-blue-400 underline'>
-                            <img src={gMailIcon} alt="gmail-icon" className='object-cover w-12'/>
+                            <img src={gMailIcon} alt="gmail-icon" className='object-cover w-12' />
                           </a>
                           <br />
                           Visit my GitHub Profile <a href="https://github.com/harshkumar123456" className='text-blue-400 underline'>
-                            <img src={gitHubIcon} alt="github-icon" className='object-cover w-12'/>
+                            <img src={gitHubIcon} alt="github-icon" className='object-cover w-12' />
                           </a>
                           <br />
                           Connect @ Linkdin <a href="https://www.linkedin.com/in/harsh-kumar-158634233/" className='text-blue-400 underline'>
-                            <img src={linkedinIcon} alt="linkedin-icon" className='object-cover w-12'/>
+                            <img src={linkedinIcon} alt="linkedin-icon" className='object-cover w-12' />
                           </a>
                           <br />
                           <br />
@@ -171,66 +133,10 @@ function App() {
 
             <div className="line-break m-4 mx-8 h-1 rounded-full bg-red-500" style={lineBreakStyle}></div>
 
-            <div className="info-card flex px-2 py-4 rounded-lg bg-[#363C43] text-[#969696]" style={infoCardStyle}>
-              <div className="question-mark-and-reactagles me-3 min-w-[16px] max-h-fit">
-                <img src={questionMark} alt="image" className='pb-12' />
-                <img src={rectangles} alt="image" />
-              </div>
-
-              <div className="info">
-                <div className="controls rounded-2xl p-1 me-6 mb-4 text-center grid gap-4 md:flex md:flex-wrap md:justify-between">
-                  <button className='px-9 py-3 rounded-xl bg-[#171717] text-white' style={galleryButtonStyle}>
-                    Gallery
-                  </button>
-
-                  <div className="sub-controls flex gap-10 md:gap-8">
-                    <input
-                      type="file"
-                      accept="image/*"
-                      multiple
-                      onChange={handleImageUpload}
-                      id="fileInput"
-                      style={{ display: "none" }}
-                    />
-                    <button className='flex items-center gap-1 text-white py-2 px-4 rounded-full text-sm text-nowrap' style={addImageButtonStyle} onClick={() => document.getElementById("fileInput").click()}>
-                      <img src={plusIcon} alt="plus-icon" />
-                      Add Image
-                    </button>
-
-                    <div className="backward-forward-buttons flex gap-4">
-                      <button className='p-3 px-4 rounded-full' style={forwardBackwardButtonStyle}>
-                        <img src={backwardArrowIcon} alt="backward-arrow" width={14} height={14} />
-                      </button>
-
-                      <button className='p-3 px-4 rounded-full' style={forwardBackwardButtonStyle}>
-                        <img src={forwardArrowIcon} alt="forward-arrow" width={14} height={14} />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-
-                <div className='max-h-44 overflow-y-scroll pt-2 pe-2 grid grid-cols-3 gap-4'>
-                  <img src={placeholderImage} alt="placeholder-image" className='object-cover rounded-xl' />
-                  <img src={placeholderImage} alt="placeholder-image" className='object-cover rounded-xl' />
-                  <img src={placeholderImage} alt="placeholder-image" className='object-cover rounded-xl' />
-                  {images.map((image, index) => (
-                    <img
-                      key={index}
-                      src={image}
-                      alt={`uploaded-img-${index}`}
-                      className='object-cover rounded-xl'
-                    />
-                  ))}
-                </div>
-              </div>
-
-            </div>
-
-            <div className="line-break m-4 mx-8 h-1 rounded-full bg-red-500" style={lineBreakStyle}></div>
-
           </div>
         </div>
       </div>
+
 
     </>
   )
